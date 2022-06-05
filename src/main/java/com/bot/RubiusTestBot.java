b8f3c2d9c28a20a3aa5f80e36a9aa53c8835bbcb
Nikita <nikita258963@gmail.com>
2022-06-05 19:44:10 +0300

    Attempt 2

--- src/main/java/com/bot/RubiusTestBot.java
+++ src/main/java/com/bot/RubiusTestBot.java
@@ -15,21 +15,13 @@ public class RubiusTestBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return System.getenv("token");
-    } //healed
+    }
 
     @SneakyThrows
     @Override
 
     public void onUpdateReceived(Update update) {
-        if (update.getMessage().getText().equals("/test")) {
-            UserHelper userHelper = new UserHelper();                       // create helper for USER
-            String answer = userHelper.getUserList().get(0).getUser_name(); // Generating answer
-            execute(SendMessage
-                    .builder()
-                    .chatId(String.valueOf(update.getMessage().getChatId()))
-                    .text(answer)
-                    .build());
-        } else if (update.getMessage().getText().equals("/dice")){
+        if (update.getMessage().getText().equals("/dice")){
             SendMessage message = new SendMessage();
             message.setChatId(String.valueOf(update.getMessage().getChatId()));
             message.setText(String.valueOf(Math.round(Math.random()*(6-0.500001+1)+0.500001)));
